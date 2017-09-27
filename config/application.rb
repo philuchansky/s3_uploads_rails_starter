39e580f2ca2c0d4b721167a8797a9f3a03a90828
Philippe Luchansky <philuchansky@gmail.com>
2017-09-26 21:20:29 -0700

    add global paperclip config to application.rb

--- config/application.rb
+++ config/application.rb
@@ -7,12 +7,24 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module S3Uploads
-  class Application < Rails::Application
+	class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+		# -- all .rb files in that directory are automatically loaded.
+		
+		config.paperclip_defaults = {
+			storage: :s3,
+			s3_region: ENV['AWS_REGION'],
+			s3_credentials: {
+					:access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+					:secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+					:bucket => ENV['S3_BUCKET']
+			},
+			url: ':s3_domain_url',
+			path: '/:class/:attachment/:id_partition/:style/:filename'
+		}
   end
 end
